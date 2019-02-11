2f74ee8b1c45467849f4195d41da601d2039be8a
khalyomede <>
2019-02-11 21:52:46 +0100

    fixed code reading (from source file or path) and added debug option

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { readFileSync } from "fs";
-import * as fileExists from "file-exists";
 import * as joi from "joi";
+import * as fancyLog from "fancy-log";
 
 interface Options {
-	features: Array<Function>;
+	features?: Array<Function>;
 	target?: String;
 	validate?: Boolean;
+	debug?: Boolean;
 }
 
 /**
@@ -14,8 +15,29 @@ interface Options {
  * @param {String} source Either a code or a file path.
  * @return {String}
  */
-const getCode = (source): string =>
-	fileExists(source) ? readFileSync(source).toString() : source;
+const getCode = (source: string, options: Options): string => {
+	let code;
+
+	if (options.debug) {
+		fancyLog.info("reading source code");
+	}
+
+	try {
+		code = readFileSync(source);
+
+		if (options.debug) {
+			fancyLog.info(`code fetched from ${source}`);
+		}
+
+		code = code.toString();
+	} catch (exception) {
+		fancyLog.info("plain code read");
+
+		code = source;
+	}
+
+	return code;
+};
 
 /**
  * Parse an object and return all the keys with their values (or default values if needed).
@@ -27,18 +49,27 @@ const parseOptions = (options: Options): Options => {
 	let parsedOptions = options;
 
 	const schema = joi.object().keys({
-		features: joi
-			.array()
-			.items(joi.func())
-			.required(),
+		features: joi.array().items(joi.func()),
 		target: joi.number().integer(),
-		validate: joi.boolean()
+		validate: joi.boolean(),
+		debug: joi.boolean()
 	});
 
 	joi.validate(parsedOptions, schema);
 
-	if (!("target" in options)) {
-		parseOptions["target"] = "7.0.0";
+	if (
+		parsedOptions !== null &&
+		parsedOptions !== undefined &&
+		parsedOptions.constructor === Object
+	) {
+		if (!("target" in parsedOptions)) {
+			parseOptions["target"] = "7.0.0";
+		}
+	} else {
+		parsedOptions = {
+			features: [],
+			debug: false
+		};
 	}
 
 	return parsedOptions;
@@ -52,19 +83,16 @@ const parseOptions = (options: Options): Options => {
  * @param {Array<Function>} options.feature The list of features you want to support.
  * @param {String} options.target The PHP version you want to transpile to.
  * @param {Boolean} options.validate=true Wether to parse the transpiled code or not (disabling this options will improve performance).
+ * @param {Boolean} options.debug=false Wether to display debug information on console or not.
  * @throws {TypeError} If one of the features listed in the options is not a callback.
  */
 const phpNext = (source: string, options: Options) => {
 	const parsedOptions = parseOptions(options);
-	let transpiledCode = getCode(source);
-	let index = 1;
+
+	let transpiledCode = getCode(source, options);
 
 	for (const feature of parsedOptions.features) {
-		transpiledCode = feature(transpiledCode, {
-			target: options.target
-		});
-
-		index++;
+		transpiledCode = feature(transpiledCode, options);
 	}
 
 	return transpiledCode;
